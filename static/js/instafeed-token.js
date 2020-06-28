e08a2dd83886b2de28523e187c56a4e301db53fb
NightOwlKeyboards <66804532+NightOwlKeyboards@users.noreply.github.com>
2020-06-28 15:59:44 -0500

    Update instafeed-token.js

--- static/js/instafeed-token.js
+++ static/js/instafeed-token.js
@@ -13,7 +13,33 @@ $(document).ready(function() {
             console.log('Error :: ', response);
           } else {
               var feed = new Instafeed({
-                         accessToken: response.Token
+                         accessToken: response.Token,
+		                  after: function () {
+
+                var owl = $('.owl2row-plugin');
+                owl.owlCarousel({
+                    loop: true,
+                    margin: 0,
+                    navText:['',''],
+                    nav: true,
+                    dots: false,
+                    owl2row: 'true',
+                    owl2rowTarget: 'item',
+                    owl2rowContainer: 'owl2row-item',
+                    owl2rowDirection: 'utd',
+                    responsive: {
+                        0: {
+                            items: 3
+                        },
+                        600: {
+                            items: 5
+                        },
+                        1000: {
+                            items: 10
+                        }
+                    }
+                });
+            },
                         });
               feed.run();
             }
