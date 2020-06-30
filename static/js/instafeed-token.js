3873f7d9586bca46c09ed0229faad13a9ffde5c6
NightOwlKeyboards <66804532+NightOwlKeyboards@users.noreply.github.com>
2020-06-30 18:00:47 -0500

    Update instafeed-token.js

--- static/js/instafeed-token.js
+++ static/js/instafeed-token.js
@@ -20,6 +20,7 @@ $(document).ready(function() {
 				slidesToScroll: 3,
 				infinite: true,
 				arrows: true,
+				appendArrows: ".container",
 				responsive: [
 			{
 			      	breakpoint: 1024,
