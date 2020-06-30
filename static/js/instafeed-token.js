89b17760d283aefd21e0bd04ebf2fafd10fb9c4c
NightOwlKeyboards <66804532+NightOwlKeyboards@users.noreply.github.com>
2020-06-30 11:11:03 -0500

    Update instafeed-token.js

--- static/js/instafeed-token.js
+++ static/js/instafeed-token.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
  
   // The Url from your Authorisations list  
-  var instantTokenApiUrl = 'https://ig.instant-tokens.com/users/50fb3e0c-69a2-4a5b-af45-7d25bd356f5b/instagram/17841429458967670/token?userSecret=kummbwriqu9su05ds3tcp':
+  var instantTokenApiUrl = 'https://ig.instant-tokens.com/users/50fb3e0c-69a2-4a5b-af45-7d25bd356f5b/instagram/17841429458967670/token?userSecret=kummbwriqu9su05ds3tcp';
   
 	$.ajax({
 		  url: instantTokenApiUrl,
