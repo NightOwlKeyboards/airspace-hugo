5dbf88867648f0f841e49f31a7d758886f3f941d
NightOwlKeyboards <66804532+NightOwlKeyboards@users.noreply.github.com>
2020-07-01 00:17:12 -0500

    Update instafeed-token.js

--- static/js/instafeed-token.js
+++ static/js/instafeed-token.js
@@ -21,8 +21,8 @@ $(document).ready(function() {
 				infinite: true,
 				arrows: true,
 				appendArrows: ".instagram-frame",
-				prevArrow:"<button type='button' class='slick-prev pull-left'><i class='left-arrow' aria-hidden='true'></i></button>",
-            			nextArrow:"<button type='button' class='slick-next pull-right'><i class='right-arrow' aria-hidden='true'></i></button>",
+				prevArrow: '.left-arrow',
+            			nextArrow: '.right-arrow',
 				responsive: [
 			{
 			      	breakpoint: 1024,
